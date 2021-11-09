40c6b1b6f8d0ec0b367c7b06b4704433f432b243
amattioli7 <afm45@pitt.edu>
2021-11-09 15:48:56 -0500

    Hardcoding FPS, drawing white background to screen

--- main.py
+++ main.py
@@ -6,18 +6,34 @@ import pygame
 # setting up window
 WIDTH, HEIGHT = 1000, 1000
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Python Game")
+
+# setting hardcoded fps
+FPS = 60
 
 # main function
 def main():
+
+    clock = pygame.time.Clock()
 
     # game loop
     running = True
     while running:
 
+        # cap the fps to 60
+        clock.tick(FPS)
+
         # check each pygame event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        # fill the window with white fill
+        WINDOW.fill((255, 255, 255))
+
+        # update the window
+        pygame.display.update()
+
     
     # close the window
     pygame.quit()
